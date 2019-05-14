b6fb10a1515d582b1d24f70144714691ffd170bb
yyh-gl <yhonda.95.gl@gmail.com>
2019-05-14 23:02:26 +0900

    時間によってあいさつ変わる君

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -3,14 +3,25 @@ package com.example.kotlintutorial
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var greeting = "こんにちは"
+        val trialTime = Date()
+        val calendar = GregorianCalendar()
+        calendar.time = trialTime
 
-        var greeting: String = "こんにちわ"
-        textview_hoge.text = greeting
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        if (hour in 1..11) {
+            greeting = "おはよう"
+        } else if (15 < hour) {
+            greeting = "こんばんは"
+        }
+
+        textview_greeting.text = greeting
     }
 }
