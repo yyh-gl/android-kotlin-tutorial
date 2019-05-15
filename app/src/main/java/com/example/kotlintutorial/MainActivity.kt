0275a843ebdc943f6acb73eaa4b8a099de8e9c17
yyh-gl <yhonda.95.gl@gmail.com>
2019-05-15 16:56:05 +0900

    スクロールビュー実装

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -2,31 +2,10 @@ package com.example.kotlintutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var mCount: Int = 0
-    private var mShowCount: TextView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mShowCount = findViewById(R.id.show_count)
-    }
-
-    fun showToast(view: View) {
-        val toast = Toast.makeText(
-            this, R.string.toast_message,
-            Toast.LENGTH_SHORT
-        )
-        toast.show()
-    }
-
-    fun countUp(view: View) {
-        ++mCount
-        mShowCount?.text = mCount.toString()
     }
 }
