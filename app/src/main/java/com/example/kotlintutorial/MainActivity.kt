5524088bb354b883c0cc555b1760e3873fab1ead
yyh-gl <yhonda.95.gl@gmail.com>
2019-05-15 14:14:08 +0900

    初期状態

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -6,31 +6,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    var sheepCount = 0 // 追加
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var greeting = "こんにちは"
-        val trialTime = Date()
-        val calendar = GregorianCalendar()
-        calendar.time = trialTime
-
-        val hour = calendar.get(Calendar.HOUR_OF_DAY)
-        if (hour in 1..11) {
-            greeting = "おはよう"
-        } else if (hour > 15) {
-            greeting = "こんばんは"
-        }
-
-        greeting += "ねむれませんか？"
-
-        textview.text = greeting
-
-        button.setOnClickListener {
-            val sheepText = "ひつじが${++sheepCount}匹"
-            textview.text = sheepText
-        }
     }
 }
