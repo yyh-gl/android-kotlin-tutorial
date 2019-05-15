f1323dc6e5a56b09edf29a56335f55caccaf1b7d
yyh-gl <yhonda.95.gl@gmail.com>
2019-05-15 15:25:11 +0900

    リファクタリング

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -6,12 +6,8 @@ import android.view.View
 import android.widget.Toast
 import android.widget.TextView
 
-
-
-
-
 class MainActivity : AppCompatActivity() {
-    var mCount : Int = 0
+    var mCount: Int = 0
     private var mShowCount: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +27,6 @@ class MainActivity : AppCompatActivity() {
 
     fun countUp(view: View) {
         ++mCount
-        mShowCount!!.setText(Integer.toString(mCount))
+        mShowCount!!.text = mCount.toString()
     }
 }
