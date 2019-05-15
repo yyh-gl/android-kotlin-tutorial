0f9f01d1ad229292c7409327e623f3878979f198
yyh-gl <yhonda.95.gl@gmail.com>
2019-05-16 01:20:10 +0900

    RecyclerView完成

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -1,20 +1,68 @@
 package com.example.kotlintutorial
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import java.util.LinkedList
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 
 
 class MainActivity : AppCompatActivity() {
-    private val mWordList = LinkedList<String>()
+    private val mWordList: LinkedList<String>? = LinkedList()
+    private var mRecyclerView: RecyclerView? = null
+    private var mAdapter: WordListAdapter? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        for (i in 0..19) {
-            mWordList.addLast("Word $i")
+        fab.setOnClickListener {
+            val wordListSize = mWordList?.size
+            // Add a new word to the wordList.
+            mWordList?.addLast("+ Word $wordListSize")
+            // Notify the adapter, that the data has changed.
+            if (wordListSize != null) {
+                mRecyclerView?.adapter?.notifyItemInserted(wordListSize)
+            }
+            // Scroll to the bottom.
+            if (wordListSize != null) {
+                mRecyclerView?.smoothScrollToPosition(wordListSize)
+            }
         }
+
+        for (i in 0..9) {
+            mWordList?.addLast("Word $i")
+        }
+
+        // Get a handle to the RecyclerView.
+        mRecyclerView = findViewById<View>(R.id.recyclerview) as RecyclerView?
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = mWordList?.let { WordListAdapter(this, it) }
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView?.adapter = mAdapter
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView?.layoutManager = LinearLayoutManager(this)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
--- app/src/main/java/com/example/kotlintutorial/WordListAdapter.kt
+++ app/src/main/java/com/example/kotlintutorial/WordListAdapter.kt
@@ -1,25 +1,63 @@
 package com.example.kotlintutorial
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
-import android.widget.TextView
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import java.util.*
+import android.view.LayoutInflater
 
 
-class WordListAdapter : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+class WordListAdapter(context: Context, wordList: LinkedList<String>) : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+    private var mWordList: LinkedList<String>? = null
+    private var mInflater: LayoutInflater? = null
 
-}
-
-internal class WordViewHolder : RecyclerView.ViewHolder {
-    var wordItemView: TextView? = null
-    var mAdapter: WordListAdapter? = null
-
-    fun WordViewHolder(itemView: View, adapter: WordListAdapter) {
-        super(itemView)
-        wordItemView = itemView.findViewById<View>(R.id.word)
-        this.mAdapter = adapter
+    init {
+        mInflater = LayoutInflater.from(context)
+        mWordList = wordList
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val mItemView = mInflater?.inflate(
+            R.layout.wordlist_item,
+            parent, false
+        )
+        return WordViewHolder(mItemView!!, this)
+    }
+
+    override fun getItemCount(): Int {
+        mWordList?.let {
+            return it.size
+        }
+        return 0
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val mCurrent = mWordList?.get(position)
+        holder.wordItemView?.text = mCurrent
+    }
+
+    inner class WordViewHolder(itemView: View, private val mAdapter: WordListAdapter) :
+        RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        var wordItemView: TextView? = null
+
+        init {
+            wordItemView = itemView.findViewById(R.id.word)
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(view: View?) {
+            // Get the position of the item that was clicked.
+            val mPosition = layoutPosition
+            // Use that to access the affected item in mWordList.
+            val element = mWordList?.get(mPosition)
+            // Change the word in the mWordList.
+            mWordList?.set(mPosition, "Clicked! $element")
+            // Notify the adapter, that the data has changed so it can
+            // update the RecyclerView to display the data.
+            mAdapter.notifyDataSetChanged()
+        }
+    }
+
 }
-
-
-
-
