7f4ec076084a265ebe53dbf3d58cc1f26bd312c2
yyh-gl <yhonda.95.gl@gmail.com>
2019-05-15 18:28:35 +0900

    途中段階までアップ

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -2,31 +2,68 @@ package com.example.kotlintutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Toast
+import android.content.Intent
+import android.widget.EditText
 import android.widget.TextView
+import android.R.attr.data
+
+
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
-    private var mCount: Int = 0
-    private var mShowCount: TextView? = null
+    private val LOG_TAG: String = MainActivity::class.java.simpleName
+    private var mReplyHeadTextView: TextView? = null
+    private var mReplyTextView: TextView? = null
+
+    companion object {
+        const val EXTRA_MESSAGE: String = "com.example.kotlintutorial.extra.MESSAGE"
+        const val EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"
+        const val TEXT_REQUEST: Int = 1
+    }
+
+    private var mMessageEditText: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mShowCount = findViewById(R.id.show_count)
+        mMessageEditText = findViewById(R.id.editText_main)
+        mReplyHeadTextView = findViewById(R.id.text_header_reply)
+        mReplyTextView = findViewById(R.id.text_message_reply)
     }
 
-    fun showToast(view: View) {
-        val toast = Toast.makeText(
-            this, R.string.toast_message,
-            Toast.LENGTH_SHORT
-        )
-        toast.show()
+    public override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int, data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                val reply = data?.getStringExtra(SecondActivity.EXTRA_REPLY)
+                mReplyHeadTextView?.visibility = View.VISIBLE
+                mReplyTextView?.text = reply
+                mReplyTextView?.visibility = View.VISIBLE
+            }
+        }
     }
 
-    fun countUp(view: View) {
-        ++mCount
-        mShowCount?.text = mCount.toString()
+    fun launchSecondActivity(view: View) {
+        Log.d(LOG_TAG, "Button clicked!")
+
+        val intent = Intent(this, SecondActivity::class.java)
+        val message = mMessageEditText?.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
+
+        startActivity(intent)
+        startActivityForResult(intent, TEXT_REQUEST)
     }
+
 }
---
+++ app/src/main/java/com/example/kotlintutorial/SecondActivity.kt
@@ -0,0 +1,36 @@
+package com.example.kotlintutorial
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.EditText
+import android.content.Intent
+import android.view.View
+
+class SecondActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"
+    }
+
+    private var mReply: EditText? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val message: String = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+        val textView: TextView? = findViewById(R.id.text_message)
+        textView?.text = message
+
+        mReply = findViewById(R.id.editText_second)
+
+    }
+
+    fun returnReply(view: View) {
+        val reply = mReply?.text.toString()
+        val replyIntent = Intent()
+        replyIntent.putExtra(MainActivity.EXTRA_REPLY, reply)
+        setResult(RESULT_OK, replyIntent)
+        finish()
+    }
+}
